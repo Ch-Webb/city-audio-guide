8d1c2cb7e2f66066a5760748f7bc1d8eb993f772
Mariana Capdevielle Schurmann <marivielle@gmail.com>
2020-02-26 11:09:18 +0000

    Initial kinda working layout
    Needs testing on different devices, however initial xamarin.forms working xaml with veeery basic main page, to be connected with backend and connect with other pages (all work done on mainPage.xaml)
    
    Will need image files to be uploaded to android -> resources -> drawable

---
+++ Audio_Guide/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Audio_Guide.Services;
+using Audio_Guide.Views;
+
+namespace Audio_Guide
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
