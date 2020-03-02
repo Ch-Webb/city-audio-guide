561f120a5766541611287563a95e531c6bff6009
Princess-10 <44162753+Princess-10@users.noreply.github.com>
2020-03-02 22:39:48 +0000

    I tried really hard... There's 2 errors in each
    pretty much the same errors--> HELP :)

---
+++ PopupDemo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PopupDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        { await PopupNavigation.PushAsync(new ShowPrivacy()); }
+    }
+}
---
+++ PopupDemo/ShowPrivacy.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PopupDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowPrivacy : PopupPage
+    {
+        public ShowPrivacy()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        { await PopupNavigation.PopAsync(true); }
+    }
+}
