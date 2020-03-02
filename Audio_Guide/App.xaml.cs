7b83e27bd7f89be7c1050bbd45fce9f60546e875
Mariana Capdevielle Schurmann <marivielle@gmail.com>
2020-03-02 16:04:44 +0000

    Files uploaded, more items buttons woking
    Only xamarin.forms, more items buttons loading, navigation page applied to app C# page

--- Audio_Guide/App.xaml.cs
+++ Audio_Guide/App.xaml.cs
@@ -14,7 +14,7 @@ namespace Audio_Guide
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
