912dd1e2fcfe1a7b7966c18b46236b55fad03846
Mariana Capdevielle Schurmann <marivielle@gmail.com>
2020-03-02 16:55:21 +0000

    Hopefully easier to understand

---
+++ Actual files changed/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Audio_Guide.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AboutPage());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AboutPage());
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AboutPage());
+        }
+
+
+    }
+}
