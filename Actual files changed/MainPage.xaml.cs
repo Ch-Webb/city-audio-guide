647c479ee0b54c9bc4ca12c51af74d130094ac59
Mariana Capdevielle Schurmann <marivielle@gmail.com>
2020-03-09 12:55:44 +0000

    New files with mapsui.forms

--- Actual files changed/MainPage.xaml.cs
+++ Actual files changed/MainPage.xaml.cs
@@ -2,12 +2,35 @@
 using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Mapsui.Projection;
+using BruTile.Predefined;
+
+using System.Diagnostics;
+using Mapsui.Geometries;
+using Mapsui.Layers;
+using Mapsui.Providers;
+using Mapsui.Styles;
+using Mapsui.Utilities;
+using Mapsui;
+using Mapsui.UI;
+
+using Mapsui.Rendering.Skia;
+
+using Mapsui.UI.Forms;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Net.Http;
+using BruTile;
+using BruTile.Web;
+
 
 namespace Audio_Guide.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
+    //[DesignTimeVisible(true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
 
@@ -15,7 +38,17 @@ namespace Audio_Guide.Views
         {
             InitializeComponent();
 
+            var mapControl = new MapsUIView();
+            mapControl.NativeMap.Layers.Add(OpenStreetMap.CreateTileLayer());
+            
+
+            //mapControl.NativeMap.NavigateTo(mapControl.NativeMap.Resolutions[9]);
+
+            ContentGrid.Children.Add(mapControl);
+            mapControl.NativeMap.Limiter = new ViewportLimiterKeepWithin();
+
         }
+
 
         private void Button_Clicked(object sender, EventArgs e)
         {
